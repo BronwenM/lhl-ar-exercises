6435855e621ac10bb48c94c476a50b525bff590f
BronwenM <bronwen.marchant@gmail.com>
2024-11-13 17:18:10 -0800

    Ex7: Added validation for creating records

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,11 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter a store name:"
+print "> "
+user_store_name = gets.chomp
+
+user_store = Store.create(name: user_store_name)
+
+puts "new store is valid? #{user_store.valid?}"
+puts user_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_carry_mens_or_womens_apparel
+
+  def must_carry_mens_or_womens_apparel
+    if !(mens_apparel.present? || womens_apparel.present?)
+      errors.add(:apparel_type, "store must carry mens or womens apparel (mens_apparel: #{mens_apparel.present?}, womens_apparel: #{womens_apparel.present?})")
+    end
+  end
 end
