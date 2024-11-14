cad329dd43bc07bffb99add59db27bae9d149060
BronwenM <bronwen.marchant@gmail.com>
2024-11-13 16:30:20 -0800

    Ex5: Outputted total and average revenue, and num stores making over 1M

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total annual revenue: #{Store.sum(:annual_revenue)}"
+puts "Average annual revenue: #{Store.average(:annual_revenue)}"
+puts "Number of stores making over $1M: #{Store.where("annual_revenue > 1000000").count()}"
